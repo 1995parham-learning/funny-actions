aa066fe2c7833e11123d457f0cef774f04d96716
Parham Alvani <parham.alvani@gmail.com>
2025-02-03 18:40:24 +0000

    feat: add code to generate report

---
+++ report/main.py
@@ -0,0 +1,8 @@
+def main():
+    print("# Actions for Fun")
+    print("I am reporting information to show on Github Action")
+    print("Parham 🐼 and Elahe 🌰")
+
+
+if __name__ == "__main__":
+    main()
