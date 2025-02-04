a0b18bac66b00e43ccac3e5ef691535e2137bfaf
Parham Alvani <parham.alvani@gmail.com>
2025-02-04 04:41:01 +0000

    feat: add issue parsing example

---
+++ issue/main.py
@@ -0,0 +1,14 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser("issue")
+    parser.add_argument("title", help="GitHub Issue Title", type=str)
+    parser.add_argument("body", help="GitHub Issue Body", type=str)
+
+    args = parser.parse_args()
+
+    print(f"Thanks for creating an issue name {args.title}, we are working on it.")
+
+
+if __name__ == "__main__":
+    main()
